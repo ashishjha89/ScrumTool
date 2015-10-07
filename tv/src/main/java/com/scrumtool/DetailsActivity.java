e3f88db8678126cd570827dea3f747d2588b9307
ashishjha89 <ashish.jha@AMSBGNB485.tpvision.com>
2015-10-07 18:00:18 +0200

    Checking1

--- tv/src/main/java/com/scrumtool/DetailsActivity.java
+++ tv/src/main/java/com/scrumtool/DetailsActivity.java
@@ -20,9 +20,14 @@ import android.os.Bundle;
 /*
  * Details activity class that loads LeanbackDetailsFragment class
  */
+
+
+
 public class DetailsActivity extends Activity {
     public static final String SHARED_ELEMENT_NAME = "hero";
     public static final String MOVIE = "Movie";
+
+    private static final String TAG = "DetailsActivity";
 
     /**
      * Called when the activity is first created.
